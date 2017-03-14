1f25b3c09acd36f93f4066c379e44d23196b0a00
Nadine Mansour <nadine.mansour94@gmail.com>
2017-03-14 10:29:52 +0200

    JQuery: Adding a new comment to the comments section in the correct place and handling the default value of the input tag.

---
+++ js/article.js
@@ -0,0 +1,43 @@
+var input = $('input');
+input.each(function(){
+    $(this).data('default', $(this).val());
+}).focus(function(){
+    if($(this).val() == $(this).data('default')){
+        $(this).val("");
+    }            
+}).blur(function(){
+    if(!$(this).val()){
+        $(this).val($(this).data('default'));
+    }
+}).keypress(function (e) {
+  if (e.which == 13) {
+    //Enter is pressed
+    if($(this).val()){
+    	addComment($(this).val());
+    }
+    return false;    
+  }
+});  
+
+
+//Method to ad a new comment to the list
+function addComment(comment) {
+    console.log(comment);
+    var text = '<li>'+
+					'<div class="comment-container">'+
+						'<div class="comment-img"><img src="images/tony.jpg" class="comment-img"></div>'+
+						'<div class="comment-text">'+
+							'<div class="comment-header">'+
+								'<p class="comment-name">TONY STARK</p>'+
+								'<i class="fa fa-calendar-o comment-header-text"></i>'+
+								'<p class="comment-header-text"> 1 minute ago</p>'+
+							'</div>'+
+							'<p class="comment-content">'+comment+'</p>'+
+						'</div>'+
+						'<div class="clear"></div>'+
+					'</div>'+		
+				'</li>';
+	$(".comments-container ul .add-comment-li").before(text);
+	$(".comments-container ul .add-comment-li input").val($(this).data('default'));
+}  
+
